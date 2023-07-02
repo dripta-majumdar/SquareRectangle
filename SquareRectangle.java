aa24e3911168c2e262ea7efcc9c2985f004acdd1
“dripta-majumdar” <driptamajumdar3002@gmail.com>
2023-07-02 14:50:56 +0530

    A code of Square triangle

---
+++ SquareRectangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class SquareRectangle {
+    public static void main(String[]args)
+    {
+        Scanner x=new Scanner(System.in);
+        float a=x.nextFloat();
+        float b=x.nextFloat();
+        if(a==b)
+        {
+            System.out.println("square");
+        }
+        else
+        {
+            System.out.println("rectangle");
+        }
+
+    }
+}
